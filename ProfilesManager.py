9eade9e2193c39271f8c473a80bbfb08d1154c7d
hamza <i230510@isb.nu.edu.pk>
2024-10-14 08:50:03 +0500

    chrome is almost setup. just proxies are left

--- ProfilesManager.py
+++ ProfilesManager.py
@@ -3,7 +3,7 @@ from PySide6 import QtCore
 from selenium import webdriver
 from multiprocessing import  Manager, Process
 import time
-
+import os
 
 
 
@@ -13,17 +13,28 @@ def launch_chrome(profile, profiles_path):
 
     # Optional: You can set Chrome options if needed
     chrome_options = webdriver.ChromeOptions()
-
-    chrome_options.add_argument(f"--user-data-dir={profiles_path}\\{profile['id']}")
+    # user_agent = profile["user_agent"]
+    user_agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0 (Edition Yx 05)"
+    chrome_options.add_argument(f"--user-data-dir={profiles_path}\\data\\profiles\\{profile['id']}")
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument(f"--user-agent={user_agent}")
+
     chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+
 
     # Create a new instance of the Chrome driver
     driver = webdriver.Chrome(options=chrome_options)
+    # get current working directory from os
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+
 
     # pre processors
-    if ((str(profile["module"])).lower() == "google"):
+    if ((str(profile["module_name"])).lower() == "google"):
         pass
 
     flag = True
@@ -62,7 +73,7 @@ class ProfilesManager:
 
 
 
-    def launch_profile(self, profiler, profiles_path = "F:\\profiles"):
+    def launch_profile(self, profiler, profiles_path = os.getcwd()):
         """Launch a profile in a separate process."""
         print(dict(profiler))
 
