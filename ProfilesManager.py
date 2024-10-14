69bf4c6d64bc9444c4843a289bc74633b88b3e04
hamza <i230510@isb.nu.edu.pk>
2024-10-14 08:20:44 +0500

    multiprocessing fixed

--- ProfilesManager.py
+++ ProfilesManager.py
@@ -1,64 +1,71 @@
-from distutils.command.check import check
 
 from PySide6 import QtCore
 from selenium import webdriver
-from multiprocessing import  Manager, Process, Queue
+from multiprocessing import  Manager, Process
 import time
 
-def test_process():
-    print("Test process started.")
-    time.sleep(5)
-    print("Test process finished.")
 
+
+
+def launch_chrome(profile, profiles_path):
+    """Function to launch Chrome browser using Selenium."""
+    print("Launching Chrome...")
+
+    # Optional: You can set Chrome options if needed
+    chrome_options = webdriver.ChromeOptions()
+
+    chrome_options.add_argument(f"--user-data-dir={profiles_path}\\{profile['id']}")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_experimental_option("detach", True)
+
+    # Create a new instance of the Chrome driver
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # pre processors
+    if ((str(profile["module"])).lower() == "google"):
+        pass
+
+    flag = True
+    while flag:
+        # noinspection PyStatementEffect
+        try:
+            driver.title
+        # exception will raise when browser is closed
+        except Exception as e:
+            print("Chrome driver has been closed.")
+            flag = False
+            driver.quit()
+
+        time.sleep(2)  # Check every 2 seconds
 
 # multi processing manager
 class ProfilesManager:
     def __init__(self):
+        self.manager = Manager()
         self.profiles = []
         self.drivers = []
         self.check_processes()
 
 
     def check_processes(self):
-        """Check if processes are alive and clear the queue if closed."""
         for idx, process in enumerate(self.profiles):
 
             if not process.is_alive():
                 print("Checking processes...")
                 process.join()  # Ensure the process resources are cleaned up
                 self.profiles.remove(process)  # Remove closed process from the list\
-                # self.drivers[idx].quit()  # Quit the corresponding drivern to
-                # del self.drivers[idx]  # Remove the corresponding driver from the list
-                print("Removed closed process from the queue.")
+                print("Removed closed process from the list.")
 
         QtCore.QTimer.singleShot(2000, self.check_processes)
 
-    def launch_chrome(self, profile):
-        """Function to launch Chrome browser using Selenium."""
-        print("Launching Chrome...")
-        # print(profile)
-
-        # Optional: You can set Chrome options if needed
-        chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument("user-data-dir=selenium")
-        # chrome_options.add_argument("--profile-directory=F:\profiles\selenium-profile")
-        chrome_options.add_argument("--user-data-dir=F:\profiles\selenium-profile\data")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_experimental_option("detach", True)
-
-        # Create a new instance of the Chrome driver
-        driver = webdriver.Chrome(options=chrome_options)
-
-        # Open a webpage (e.g., Google)
-        driver.get("https://www.google.com")
-        self.drivers.append(driver)
 
 
 
-    def launch_profile(self, profiler):
-        print(dict(profiler))
+    def launch_profile(self, profiler, profiles_path = "F:\\profiles"):
         """Launch a profile in a separate process."""
-        chrome_process = Process(target=self.launch_chrome, args=(dict(profiler),))
+        print(dict(profiler))
+
+        chrome_process = Process(target=launch_chrome, args=(dict(profiler), profiles_path))
         chrome_process.start()
         self.profiles.append(chrome_process)
